4e4a22f2f26f1da6930f0e6e00ac145570c4f54e
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-09-23 15:33:52 -0700

    add

--- rangi/settings.py
+++ rangi/settings.py
@@ -80,3 +80,7 @@ USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://your-production-site.com",
+# ]
