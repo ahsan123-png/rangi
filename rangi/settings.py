78b8e946001d3fab9b87d69efbfa82183eb0cfd4
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-09-24 07:17:41 -0700

    CORS settings

--- rangi/settings.py
+++ rangi/settings.py
@@ -80,9 +80,16 @@ USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "https://your-production-site.com",
-# ]
-CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    # "https://your-production-site.com",
+]
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+    'x-csrftoken',
+    'accept',
+    'origin',
+    'user-agent',
+]
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
