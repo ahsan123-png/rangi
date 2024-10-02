9aecc56c4fc34ca6b23a4b8f6f0d83a154e1a0af
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-10-02 13:29:58 -0700

    add vercel settings

--- rangi/settings.py
+++ rangi/settings.py
@@ -97,10 +97,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = False 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    # "https://your-production-site.com",
+    "https://thefixit4u.com", 
 ]
 CORS_ALLOW_HEADERS = [
     'authorization',
